967359ac0c43996f91d96250e6eb5f57fed20e34
Josumaru <jokosupriyanto1712@gmail.com>
2023-10-14 01:45:17 +0700

    Saving 3 Streak Today

--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
-```js 
+```js
+/*
+
+This is simple program to save your GitHub Streak
+Make streak.sh
+ executeable and start automatically when reboot, adjust your Desktop Environment
+Currently only work on linux or run using WSL and also Android terminal emulator and Linux environment app
+                                                        
+*/
 const [streak, setStreak] = useState(3);
 const date = 14;
 const streakSaver = () => {
--- streak.sh
+++ streak.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-streak=$(cat readme.md | sed -n "2p" | grep -oE "[0-9]+")
-validator=$(cat readme.md | sed -n "3p" | grep -oE "[0-9]+")
-date=$(date +%d)
+line=$(grep -n "useState" readme.md | cut -d ":" -f 1)
+streak=$(cat readme.md | sed -n "${line}p" | grep -oE "[0-9]+")
+validator=$(cat readme.md | sed -n "$((line+1))p" | grep -oE "[0-9]+")
+day=$(date +%d)
 streak=$((streak+1))
 messages="Saving $streak Streak Today"
+echo $line
 
 if [[ -f .env ]];then
     source .env
-    if [[ $date -ne $validator ]]; then
-        sed -i "2s/.*/const [streak, setStreak] = useState($streak);/" readme.md
-        sed -i "3s/.*/const date = $date;/" readme.md
+    if [[ $day -ne $validator ]]; then
+        sed -i "${line}s/.*/const [streak, setStreak] = useState($streak);/" readme.md
+        sed -i "$((line+1))s/.*/const date = $day;/" readme.md
         git add .
         git commit -m "$messages"
         git push https://$USERNAME:$PASSWORD@github.com/$USERNAME/streak.git
+        echo "$messages at $(date)" >> streak.log
     else
-        echo "Already Push today"
+        echo "Reboot at $(date) and Already Save Today" >> streak.log
     fi
 else
     echo -e "Set up your .env with :\nUSERNAME=YOUR_GITHUB_USERNAME\nPASSWORD=YOUR_GITHUB_PASSWORD"
