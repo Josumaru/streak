327afad99e7f7d001a65bec6bc29de92c7408def
Josumaru <jokosupriyanto1712@gmail.com>
2023-10-14 00:30:37 +0700

    Saving 38 Streak Today

---
+++ readme.md
@@ -0,0 +1,11 @@
+```js 
+const [streak, setStreak] = useState(38);
+const date = 14;
+const streakSaver = () => {
+    setIsStreak(streak + 1);
+    console.log(`Current streak is ${streak}`);
+}
+
+streakSaver();
+
+```
---
+++ streak.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+streak=$(cat readme.md | sed -n "2p" | grep -oE "[0-9]+")
+validator=$(cat readme.md | sed -n "3p" | grep -oE "[0-9]+")
+date=$(date +%d)
+streak=$((streak+1))
+messages="Saving $streak Streak Today"
+
+if [[ -f .env ]];then
+    source .env
+    if [[ $date -ne $validator ]]; then
+        sed -i "2s/.*/const [streak, setStreak] = useState($streak);/" readme.md
+        sed -i "3s/.*/const date = $date;/" readme.md
+        git add .
+        git commit -m "$messages"
+        git push https:$USERNAME:$$PASSWORD@github.com/$USERNAME/streak-saver.git
+    else
+        echo "Already Push today"
+    fi
+else
+    echo -e "Set up your .env with :\nUSERNAME=YOUR_GITHUB_USERNAME\nPASSWORD=YOUR_GITHUB_PASSWORD"
+fi
+
+
+# if [[ $date -ne $validator ]]; then
+#     sed -i "2s/.*/const [streak, setStreak] = useState($streak);/" readme.md
+#     sed -i "3s/.*/const date = $date;/" readme.md
+# fi
+
+
+# echo $messages
+# git add .
+# git commit -m "$messages"
+# git push
